9978c89469b88f3cdd6d4abbbd0998e148955013
Mohammad <mohammaddhorajiwala11@gmail.com>
2026-02-07 17:42:50 +0530

    initial commit

---
+++ app/js/data.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    name: "Classic Copper Stockpot",
+    category: "cookware",
+    price: 189,
+    brand: "Prestige",
+    image: "assets/images/products/stockpot.jpg",
+  },
+  {
+    id: 2,
+    name: "Non-Stick Fry Pan",
+    category: "cookware",
+    price: 89,
+    brand: "Pigeon",
+    image: "assets/images/products/frypan.jpg",
+  },
+  {
+    id: 3,
+    name: "Air Fryer Pro",
+    category: "appliances",
+    price: 499,
+    brand: "Bajaj",
+    image: "assets/images/products/airfryer.jpg",
+  },
+  {
+    id: 4,
+    name: "Ceramic Dinner Set",
+    category: "tableware",
+    price: 299,
+    brand: "Milton",
+    image: "assets/images/products/dinnerset.jpg",
+  },
+];
